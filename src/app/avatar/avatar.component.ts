8d19d034529583c7af40efe897aac96e644ae646
Aley Eladawy <aeeladawy@gmail.com>
2017-06-04 03:10:47 +0200

    Avatar movement bounded correctly (transition needs fixing)

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -24,79 +24,100 @@ import { Cell } from "../classes/cell/cell"
 })
 
 export class AvatarComponent implements OnInit {
-  size:{width:string, height:string};  
-  part:any;
+  size: { width: string, height: string };
+  part: any;
   @Input() map: Array<Array<[Cell, boolean]>>;
-  @Input() gridLocation:{top:number, left:number};
-  @Input() startPosition: {x:number, y:number};
+  @Input() gridLocation: { top: number, left: number };
+  @Input() currentPosition: { x: number, y: number };
 
-constructor(private el: ElementRef, 
-  private renderer: Renderer) {
-}
+  constructor(private el: ElementRef,
+    private renderer: Renderer) {
+  }
 
-ngOnInit() {
-      this.part = this.el.nativeElement;
-     
-      // this.renderer.setElementyStyle(this.part, 'scale')
+  ngOnInit() {
+    this.part = this.el.nativeElement;
 
-}
+    // this.renderer.setElementyStyle(this.part, 'scale')
 
-ngAfterViewChecked(){
+  }
+
+  ngAfterViewChecked() {
     console.log("EHHH");
-    if(typeof this.size === "undefined" || (typeof this.gridLocation === "undefined"))
+    if (typeof this.size === "undefined" || (typeof this.gridLocation === "undefined"))
       return;
     console.log(this.size);
     console.log(this.gridLocation)
-    console.log(this.startPosition)
+    console.log(this.currentPosition)
     let width = +this.size.width.split('px')[0]
     let height = +this.size.height.split('px')[0]
     let dimension = this.part.getBoundingClientRect().width
-     this.renderer.setElementStyle(this.part, 'left', (this.gridLocation.left+width/2-dimension/2 + this.startPosition.x*(1+width))+"px")
-     this.renderer.setElementStyle(this.part, 'top', (this.gridLocation.top+height/2-dimension/2 + this.startPosition.y*(1+height))+"px")
-    console.log((this.gridLocation.left+width/2-dimension/2 + this.startPosition.x*width))
-    console.log((this.gridLocation.top+height/2-dimension/2 + this.startPosition.y*height))
+    this.renderer.setElementStyle(this.part, 'left', (this.gridLocation.left + width / 2 - dimension / 2 + this.currentPosition.x * (1 + width)) + "px")
+    this.renderer.setElementStyle(this.part, 'top', (this.gridLocation.top + height / 2 - dimension / 2 + this.currentPosition.y * (1 + height)) + "px")
+    console.log((this.gridLocation.left + width / 2 - dimension / 2 + this.currentPosition.x * width))
+    console.log((this.gridLocation.top + height / 2 - dimension / 2 + this.currentPosition.y * height))
     console.log("omar essam eldin hassan")
-}
-// @HostBinding('style.transform') c_colorrr = "red"; 
+    console.log(this.currentPosition)
+  }
+  // @HostBinding('style.transform') c_colorrr = "red"; 
 
-@HostListener('document:keyup', ['$event'])
-handleKeyboardEvent(event: KeyboardEvent) {
-  console.log(event);
-  this.move(event.keyCode)
-}
-move = (keyCode) => {
-  let compuStyle = window.getComputedStyle(this.part);
-  let origXVal = compuStyle.getPropertyValue("transform").split('(')[1];
-  origXVal = origXVal.split(')')[0];
-  let origX = origXVal.split(',');
-  let width = +this.size.width.split('px')[0] + 1;
-  console.log("WIDTH: "+width);
-  let height = +this.size.height.split('px')[0] + 1;  
-  switch (keyCode) {
-    case (37):
-      var newX = +origX[4] - width;
-      var oldY = +origX[5];
-      this.renderer.setElementStyle(this.part, 'transform', 'translate(' + newX + 'px,' + oldY + 'px)');
-      break;
+  @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    console.log(event);
+    this.move(event.keyCode)
+  }
+  move = (keyCode) => {
+    let compuStyle = window.getComputedStyle(this.part);
+    let origXVal = compuStyle.getPropertyValue("transform").split('(')[1];
+    origXVal = origXVal.split(')')[0];
+    let origX = origXVal.split(',');
+    let width = +this.size.width.split('px')[0] + 1;
+    console.log("WIDTH: " + width);
+    let height = +this.size.height.split('px')[0] + 1;
+    switch (keyCode) {
+      case (37):
+        if (!(this.map[this.currentPosition.y][this.currentPosition.x][0].left))
+          return;
 
-    case (38):
-      var newY = +origX[5] - height;
-      var oldX = +origX[4];
-      this.renderer.setElementStyle(this.part, 'transform', 'translate(' + oldX + 'px,' + newY + 'px)');
-      break;
+        var newX = +origX[4] - width;
+        var oldY = +origX[5];
+        this.renderer.setElementStyle(this.part, 'transform', 'translate(' + newX + 'px,' + oldY + 'px)');
+                  this.currentPosition.x--;
+
+        break;
+
+      case (38):
+       if (!(this.map[this.currentPosition.y][this.currentPosition.x][0].up))
+            return;
+         
+        var newY = +origX[5] - height;
+        var oldX = +origX[4];
+        this.renderer.setElementStyle(this.part, 'transform', 'translate(' + oldX + 'px,' + newY + 'px)');
+                  this.currentPosition.y--;
+
+        break;
 
 
-    case (39):
-       newX = +origX[4] + width;
-       oldY = +origX[5];
-      this.renderer.setElementStyle(this.part, 'transform', 'translate(' + newX + 'px,' + oldY + 'px)');
-      break;
+      case (39):
+      if (!(this.map[this.currentPosition.y][this.currentPosition.x][0].right))
+            return;
+          
+        newX = +origX[4] + width;
+        oldY = +origX[5];
+        this.renderer.setElementStyle(this.part, 'transform', 'translate(' + newX + 'px,' + oldY + 'px)');
+                 this.currentPosition.x++;
 
-    case (40):
-       newY = +origX[5] + height;
-       oldX = +origX[4];
-      this.renderer.setElementStyle(this.part, 'transform', 'translate(' + oldX + 'px,' + newY + 'px)');
-      break;
-  }  
-}
-}
+        break;
+
+      case (40):
+       if (!(this.map[this.currentPosition.y][this.currentPosition.x][0].down))
+            return;
+         
+        newY = +origX[5] + height;
+        oldX = +origX[4];
+        this.renderer.setElementStyle(this.part, 'transform', 'translate(' + oldX + 'px,' + newY + 'px)');
+                 this.currentPosition.y++;
+
+        break;
+    }
+    }
+  }
